21a8d7540a68beeb7fe4095872ce234301ef074e
Juan Cabrerizo <juan@cabrerizo.es>
2021-01-23 18:51:54 +0000

    Initial structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='AWS Transcriber',
+   version='0.1.0',
+   description='<DESCRIPTION>',
+   author='Juan Cabrerizo',
+   url="https://github.com/jcabrerizo/aws-transcriber",
+   packages=['transcriber']
+
+) 

477589414b0ca838c91db77a52bb48dd03262fcf
Juan Cabrerizo <juan@cabrerizo.es>
2021-01-25 20:27:35 +0000

    Processing as input folder. Pending add params and fix path issues

--- transcriber/__main__.py
+++ transcriber/__main__.py
@@ -8,7 +8,7 @@ from formatter.htmlFormatter import HtmlFormatter
 logging.basicConfig(level=logging.DEBUG)
 printScreenMessages = True
 path = "../resources/asrOutput.json"
-
+openInBrowser = False
 
 def screenMessage(msg):
     if printScreenMessages:
@@ -17,22 +17,29 @@ def screenMessage(msg):
 
 def main():
     screenMessage("AWS transcrip")
-    parser = TranscriptParser(path)
-    screenMessage(f"Path in parser: {parser.getJsonPath()}")
-    data = parser.parse()
-    screenMessage(f"AWS account: {data['accountId']}")
-    screenMessage(f"JobName: {data['jobName']}")
-    htmlFormatter = HtmlFormatter(data)
-    # TODO: pass format options
-    html = htmlFormatter.format()
-    # TODO: add parameter for printing it
-#    print(html)
-    # TODO: create module for file management
-    outputFile = f"./target/{data['jobName']}.html"
-    with open(outputFile, "w") as fh:
-        fh.write(html)
 
-    webbrowser.open('file://' + os.path.realpath(outputFile))
+    inputDirectory = r'../input'
+    for inputFile in os.listdir(inputDirectory):
+        if inputFile.endswith(".json"):
+            parser = TranscriptParser(f'{inputDirectory}/{inputFile}')
+            screenMessage(f"Path in parser: {parser.getJsonPath()}")
+            data = parser.parse()
+            screenMessage(f"AWS account: {data['accountId']}")
+            screenMessage(f"JobName: {data['jobName']}")
+            htmlFormatter = HtmlFormatter(data)
+            # TODO: pass format options
+            html = htmlFormatter.format()
+            # TODO: add parameter for printing it
+            #    print(html)
+            # TODO: create module for file management
+            outputFile = f"./target/{data['jobName']}.html"
+            with open(outputFile, "w") as fh:
+                fh.write(html)
+            if openInBrowser:
+                webbrowser.open('file://' + os.path.realpath(outputFile))
+        else:
+            continue
+
 
 
 if __name__ == "__main__":
